66cf06fb77e4a0c6323c24263004f13afab3ccb8
Toon Bertier <toonbertier@gmail.com>
2015-12-18 23:32:25 +0100

    wrapurl helper

---
+++ _helpers/wrapURL.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = str => {
+
+  //a tag from url
+  var matches = str.match(/http\S+/);
+  if(matches !== null) {
+
+    var wrapped = matches.map((v, i, a) => '<a href="' + v + '">' + v + '</a>');
+
+    for (var i = 0; i < matches.length; i++) {
+        str = str.replace(matches[i], wrapped[i]);
+    }
+
+  }
+
+  //a tag from hashtag
+  matches = str.match(/#(\S*)/g);
+  if(matches !== null) {
+
+    var wrapped = matches.map((v, i, a) => '<a href="http://twitter.com/hashtag/' + v.slice(1) +'">' + v + '</a>');
+
+    for (var i = 0; i < matches.length; i++) {
+        str = str.replace(matches[i], wrapped[i]);
+    }
+
+  }
+
+  return str;
+
+};
